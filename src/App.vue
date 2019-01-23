ce345ab76818d89d6f9d6ae5e965a40823e38d36
huangqj <huangqj@genehe.com>
2019-01-23 11:39:37 +0800

    add element.js

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view/> -->
+
+    <!-- <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div> -->
+    <ClinicalTemplate msg="Hello ClinicalTemplate"/>
   </div>
 </template>
 
-<style lang="less">
+<script>
+import ClinicalTemplate from "./components/ClinicalTemplate.vue";
+
+export default {
+  name: "app",
+  components: {
+    ClinicalTemplate
+  }
+};
+</script>
+
+<style>
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 #nav {
   padding: 30px;
