aaa6acb57f8ee2de68b881bd022e40eb980e1f99
huangqj <huangqj@genehe.com>
2019-01-23 11:30:01 +0800

    init and add bootstrap-vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <ClinicalTemplate msg="ClinicalTemplate"/>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import ClinicalTemplate from "@/components/ClinicalTemplate.vue"; // @ is an alias to /src
 
 @Component({
   components: {
-    HelloWorld
+    ClinicalTemplate
   }
 })
 export default class Home extends Vue {}
