a1fd6a658e4f04af7670b616e7fa80073d9bad2e
huangqj <huangqj@vienomics.com>
2019-03-20 18:37:45 +0800

    ...

--- bin/sop.py
+++ bin/sop.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import sys
 import os
-dirpath = os.path.join(os.path.dirname(os.path.abspath(__file__)),"../")
-sys.path.insert(0,dirpath)
+dirpath = os.path.join(os.path.dirname(os.path.realpath(__file__)),"../")
+sys.path.insert(0, dirpath)
 
 from sop.sop import cli
 
